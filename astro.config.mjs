495c51965f80fb5169e29176601d784a2191ee54
Sutin <poly.dev2018@gmail.com>
2023-09-05 20:36:32 +0700

    fix: add @analog/angular

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,18 @@
 import { defineConfig } from 'astro/config';
 import pandacss from '@pandacss/astro'
+import angular from '@analogjs/astro-angular';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations:[pandacss()]
+    integrations:[
+        pandacss(),
+        angular({
+            // vite: {
+            //     inlineStylesExtension: 'scss|sass|less',
+            //     ssr: {
+            //         noExternal: ['@rx-angular/**'],
+            //     }
+            // },
+        })
+    ]
 });
